beb00d45cdbad1e104249031464e93edcc4f2f8b
santmendoza <santiagomendoza1997@gmail.com>
2020-03-02 16:49:40 -0500

    Validacion
    Código "Validar"

--- oc.py
+++ oc.py
@@ -1,5 +1,27 @@
-from Operaciones import Operador 
-from sys import argv 
 
-script, conjunto1, conjunto2= argv
+from sys import argv
+
+class Validar:
+
+    def __init__(self,conjunto1):
+        self.conjunto1 = conjunto1
+
+
+    def val_sintaxis(self,conjunto):
+
+        if conjunto.count(',,')==0 and conjunto.find(',')!=1 and conjunto.rfind(',')!=len(conjunto)-2  and conjunto.find('{')==0 and conjunto.rfind('}')==len(conjunto)-1 and conjunto.count('}')==1 and conjunto.count('{')==1:
+        
+            print("¡La sintaxis ingresada es la correcta!")
+            
+        else:
+            print("error")
+            exit()
+
+
+script, conjunto1 = argv
+
+c1=Validar(conjunto1)
+c1.val_sintaxis(c1.conjunto1)
+
+
 

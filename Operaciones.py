d545bf2d2cafc8f6a0c1ae498f7c519be777bd76
cristianmercado1997 <47404910+cristianmercado1997@users.noreply.github.com>
2020-03-02 16:58:46 -0500

    Agregando

--- Operaciones.py
+++ Operaciones.py
@@ -1,3 +1,4 @@
+from Validar import Validar
 import sys
 
 class Operador():
@@ -6,15 +7,42 @@ class Operador():
         self.conjuto1=conjunto1
         
 
-
-
     def validar(self,conjunto)
-
-
-
-
-
         
+        
+        
+
+
+    def op_conjuntos(self, conjunto1, conjunto2):
+
+        p=conjunto1.replace('{','').replace('}','').replace(',',' ')
+        y=conjunto2.replace('{','').replace('}','').replace(',',' ')
+        #print(p)
+        #print(y)
+        x=p[1:len(p)]
+        a=y[1:len(y)]
+        #print(x)
+        #print(a)
+
+        lista1=x.split()
+        lista2=a.split()
+
+        #print(lista1)
+        #print(lista2)
+        papa = set(lista1)
+        arroz= set(lista2)
+        #print(papa)
+        #print(arroz)
+
+        print("***Union de conjuntos****")
+        print (papa.union(arroz))
+        print("==============================")
+
+        # Diferencia entre conjuntos
+        print("***Diferencia de conjuntos***")
+        print (papa - arroz)
+        print (arroz - papa)
+        print("==============================")
 
 
     
